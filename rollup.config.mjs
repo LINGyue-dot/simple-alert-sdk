3dc7c50cc9fdbba21195021903a140190b022c15
shunze.chen <qianlonwork@outlook.com>
2023-06-01 16:10:23 +0800

    fix: edit package.json main and types

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,21 +1,16 @@
 import commonjs from "@rollup/plugin-commonjs";
 import externals from "rollup-plugin-node-externals";
 import typescript from "@rollup/plugin-typescript";
-import path from 'path'
 export default {
   input: "src/index.ts",
   output: {
-    file:'dist/bundle.js',
+    file: "dist/index.js",
     format: "umd",
-    name:'SimpleAlertSDK'
+    name: "SimpleAlertSDK",
   },
   plugins: [
     commonjs(),
-    typescript({
-      outDir: "dist",
-      declaration: true,
-      declarationDir: "dist",
-    }),
+    typescript({ declaration: true, declarationDir: "dist" }),
     externals({
       devDeps: false, // devDependencies 类型的依赖就不用加到 externals 了。
     }),
