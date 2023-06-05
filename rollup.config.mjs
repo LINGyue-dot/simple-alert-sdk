9faf3d29ce8a7ff92cfb6eddb2e4a4af487971ae
shunze.chen <qianlonwork@outlook.com>
2023-06-05 14:11:59 +0800

    fix: change to public resolve pulgin and use browser axios

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,16 +1,24 @@
 import commonjs from "@rollup/plugin-commonjs";
-import externals from "rollup-plugin-node-externals";
 import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
 export default {
   input: "src/index.ts",
   output: {
     file: "dist/index.js",
     format: "umd",
     name: "SimpleAlertSDK",
+    globals: {
+      axios: "axios", // 将 Axios 设置为全局变量
+      "error-stack-parser": "ErrorStackParser",
+    },
   },
   plugins: [
     commonjs(),
     typescript({ declaration: true, declarationDir: "dist" }),
-    externals()
+    nodeResolve({
+      browser: true,
+    }),
+    json(),
   ],
 };
--- src/transport.ts
+++ src/transport.ts
@@ -6,6 +6,23 @@ import { genErrorDataHash } from "./utils";
 export function send(errorData: ErrorDataType) {
   const hash = genErrorDataHash(errorData);
 
+  console.log(axios);
+  axios.request({
+    method: "post",
+    url: `${AlertStore.options.baseUrl}/report`,
+    data: {
+      alertData: {
+        project: AlertStore.options.project,
+        env: AlertStore.options.env,
+        version: AlertStore.options.version,
+        type: errorData.type,
+        data: JSON.stringify(errorData),
+        fixed: false,
+        hash,
+      },
+    },
+  });
+
   axios
     .post(`${AlertStore.options.baseUrl}/report`, {
       alertData: {
